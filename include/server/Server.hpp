8601e7252954067aa0a2e0530ebec81c35380cde
YoshiFrancis <francis.ozua@gmail.com>
2025-06-29 15:09:36 -0400

    created initial Server class in .hpp with projected functions

--- include/server/Server.hpp
+++ include/server/Server.hpp
@@ -2,7 +2,10 @@
 #define SERVER_HPP
 
 #include "defines.hpp"
+#include "zmq.hpp"
 
+#include <span>
+#include <thread>
 #include <unordered_map>
 #include <vector>
 
@@ -14,11 +17,39 @@ private:
   std::unordered_map<pirates::game_id, pirates::game_info> owned_games;
   pirates::server_info info;
 
+  bool alive = true;
+
+  std::thread user_input_thread;
+  std::thread ship_listener_thread;
+  std::thread client_listener_thread;
+  std::thread core_thread;
+
+  zmq::context_t context;
+
+  // socket communicating with all ships following this ship
+  zmq::socket_t ship_router;
+  // socket communicating with all mates on this ship
+  zmq::socket_t client_router;
+  // socket communicating from core_thread with the ship, client, and user input
+  // threads
+  zmq::socket_t core_pull;
+  // socket communicating with ship this ship follows (if there is any)
+  zmq::socket_t ship_dealer;
+
 public:
   Server(int port_number);
   ~Server();
 
 private:
+  void user_input_task();
+  void ship_listener_task();
+  void client_listener_task();
+  void core_task();
+
+  // Ship can only get commands from user
+  void handle_user_input(std::span<zmq::message_t> input);
+  void handle_sub_ship_input(std::span<zmq::message_t> input);
+  void handle_crewmate_input(std::span<zmq::message_t> input);
 };
 
 #endif
